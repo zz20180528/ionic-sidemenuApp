5fb9c0aa6de679417aece9277aecffac45198c9c
jaeha.kim <zz20180528@gmail.com>
2018-05-30 21:03:23 +0900

    수정

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
+
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -21,8 +23,10 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Home', component: HomePage }
+      ,{ title: 'List', component: ListPage }
+      ,{ title: 'ActionSheet', component: 'ActionSheetPage' }  //lazy loding
+
     ];
 
   }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {ModalController, NavController} from 'ionic-angular';
+import {Profile} from "../../interfaces/Profile";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public  profile = {} as Profile ;
 
+  constructor(public navCtrl: NavController
+            ,public modalCtrl : ModalController  //modal 컨트롤러 관련 추가
+       ) {
+
+  }
+
+  //modal 페이지 띄우기
+  modal(){
+     let modal = this.modalCtrl.create('ModalPage') //modal 페이지 component name 을 적어줌
+     modal.onDidDismiss( data => {
+      // console.log( data );
+       //console.log( data.actionSwitch );
+       //console.log( data.name );
+       //console.log( data.gender );
+       //console.log( data.domestic );
+       //console.log( data.startDate );
+
+       this.profile.actionSwitch = data.actionSwitch;
+       this.profile.name = data.name;
+       this.profile.gender = data.gender;
+       this.profile.domestic = data.domestic;
+       this.profile.startDate = data.startDate;
+
+     }); // arrow  function
+      modal.present();
   }
 
 }
